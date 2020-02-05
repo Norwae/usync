e74b6fdbb5516e8feb8eb690005b77f5c5a2cb0c
Elisabeth Schulz <elisabeth.schulz@codecentric.de>
2020-02-05 16:29:53 +0100

    Local-only sync

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,72 @@
 use std::io::Error;
+use crate::tree::{DirectoryEntry, Named};
+use std::path::{Path, PathBuf};
+use std::fs::create_dir;
 
 mod config;
 mod tree;
 
+fn find_named<'a, T : Named>(all: &'a mut [T], name: &String) -> Option<&'a mut T> {
+    for candidate in all {
+        if candidate.name() == name {
+            return Some(candidate)
+        }
+    }
+    None
+}
+fn copy_file<P: AsRef<Path>>(target: P, source: P) -> Result<u64, Error> {
+    std::fs::copy(source, target)
+}
+
+fn copy(root: &Path, target: &mut DirectoryEntry, source: &DirectoryEntry) -> Result<(), Error> {
+    for dir in &source.subdirs {
+        let mut subdir = PathBuf::from(root);
+        subdir.push(&dir.name);
+        let subdir = subdir.as_path();
+        let partner = find_named(&mut target.subdirs, &dir.name);
+
+        let partner= match partner {
+            None => {
+                create_dir(subdir)?;
+                target.subdirs.push(DirectoryEntry::empty(subdir));
+                target.subdirs.last_mut().unwrap()
+            },
+            Some(p) => p,
+        };
+
+        if partner.hash_value != dir.hash_value {
+            copy(subdir, partner, dir)?;
+        }
+    }
+
+    for srcFile in &source.files {
+        let mut file = PathBuf::from(root);
+        file.push(&srcFile.name);
+        let file = file.as_path();
+        let partner = find_named(&mut target.files, &srcFile.name);
+
+        if partner.is_none() || {
+            let partner = partner.unwrap();
+            partner.modification_time != srcFile.modification_time ||
+            partner.file_size != srcFile.file_size ||
+            partner.hash_value != srcFile.hash_value
+        } {
+            copy_file(file, srcFile.path.as_path())?;
+        }
+    }
+
+    Ok(())
+}
 
 fn main() -> Result<(), Error>{
     let cfg = config::configure()?;
     let src = cfg.source.canonicalize()?;
-    tree::DirectoryEntry::new(src, &cfg)?;
+    let src = tree::DirectoryEntry::new(src, &cfg)?;
+    let dst = cfg.target.canonicalize()?;
+    let mut dst = tree::DirectoryEntry::new(dst, &cfg)?;
+
+    copy(cfg.target.as_path(), &mut dst, &src)?;
+
+
     Ok(())
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,7 @@
 use std::ffi::OsStr;
 use std::fs::{File, read_dir};
 use std::io::{Error, Read};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::time::SystemTime;
 
 use ring::digest::{Context, SHA256};
@@ -9,22 +9,33 @@ use crate::config::{ManifestMode, Configuration};
 
 type ShaSum = [u8; 32];
 
+pub(crate) trait Named {
+    fn name(&self) -> &String;
+}
+
 #[derive(Debug)]
 pub struct FileEntry {
-    name: String,
-    modification_time: SystemTime,
-    file_size: u64,
-    hash_value: ShaSum,
+    pub(crate) name: String,
+    pub(crate) modification_time: SystemTime,
+    pub(crate) file_size: u64,
+    pub(crate) hash_value: ShaSum,
+    pub(crate) path: PathBuf
+}
+
+impl Named for FileEntry {
+    fn name(&self) -> &String {
+        &self.name
+    }
 }
 
 
 impl FileEntry {
     pub fn new<S: AsRef<OsStr>>(path: S, config: &Configuration) -> Result<FileEntry, Error> {
-        let path = Path::new(&path);
+        let path = PathBuf::from(&path);
 
         let hash_value = if config.manifest_mode == ManifestMode::Hash {
             unsafe {
-                let file = File::open(path)?;
+                let file = File::open(path.as_path())?;
                 let mmap = memmap2::Mmap::map(&file)?;
                 hash(mmap.as_ref())?
             }
@@ -46,20 +57,37 @@ impl FileEntry {
             modification_time,
             file_size,
             hash_value,
+            path
         })
     }
 }
 
 #[derive(Debug)]
 pub struct DirectoryEntry {
-    name: String,
-    modification_time: SystemTime,
-    subdirs: Vec<DirectoryEntry>,
-    files: Vec<FileEntry>,
-    hash_value: ShaSum,
+    pub(crate) name: String,
+    pub(crate) modification_time: SystemTime,
+    pub(crate) subdirs: Vec<DirectoryEntry>,
+    pub(crate) files: Vec<FileEntry>,
+    pub(crate) hash_value: ShaSum,
+}
+
+impl Named for DirectoryEntry {
+    fn name(&self) -> &String {
+        &self.name
+    }
 }
 
 impl DirectoryEntry {
+    pub fn empty<S: AsRef<OsStr>>(path: S) -> DirectoryEntry {
+        DirectoryEntry {
+            name: filename_to_string(Path::new(&path).file_name()),
+            modification_time: SystemTime::now(),
+            subdirs: Vec::new(),
+            files: Vec::new(),
+            hash_value: hash(&[]).unwrap()
+        }
+    }
+
     pub fn new<S: AsRef<OsStr>>(path: S, config: &Configuration) -> Result<DirectoryEntry, Error> {
         let path = Path::new(&path);
         let dir = read_dir(path)?;
